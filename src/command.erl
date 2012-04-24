af77fdd54ef9823c048fcdd3e0efed4ff7461987
Beads <beads@beadsland.com>
2012-04-24 02:28:31 -0400

    double spacing after periods in license headers

--- src/command.erl
+++ src/command.erl
@@ -2,13 +2,13 @@
 %% -----------------------------------------------------------------------
 %% The contents of this file are subject to the Common Development and 
 %% Distribution License, Version 1.0 (the "License"); you may not use 
-%% this file except in compliance with the License. You should have 
+%% this file except in compliance with the License.  You should have 
 %% received a copy of the Common Development and Distribution License 
-%% along with this software. If not, it can be retrieved online at 
+%% along with this software.  If not, it can be retrieved online at 
 %% http://www.opensource.org/licenses/CDDL-1.0
 %%
 %% Software distributed under the License is distributed on an "AS IS"
-%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
 %% the License for the specific language governing rights and limitations
 %% under the License.
 %%
@@ -16,21 +16,20 @@
 %% each file and include the License file at CDDL-LICENSE.  If applicable
 %% add the following below the CDDL Header, with the fields enclosed
 %% by brackets replaced by your own identifying information:
-%% "Portions Copyrighted [year] [name of copyright owner]"
+%% "Portions Copyright [year] [name of copyright owner]"
 %%
-%% Copyright 2012 Beads D. Land-Trujillo. All Rights Reserved
+%% Copyright 2012 Beads D. Land-Trujillo.  All Rights Reserved
 %% -----------------------------------------------------------------------
 %% CDDL HEADER END
 
+%% @doc This is a preliminary draft of the command line parser for `nosh'.
 %% @author Beads D. Land-Trujillo [http://twitter.com/beadsland]
 %% @copyright 2012 Beads D. Land-Trujillo
-%% @doc This is a preliminary draft of the command line parser for `nosh'.
 %% @reference See <a href="http://sayle.net/book/basics.htm">Shell Basics</a> for overview of functionality.  (to be implemented)
 %% @reference See <a href="http://pubs.opengroup.org/onlinepubs/009695399/utilities/xcu_chap02.html">Shell Command Language</a> 
 %% for detailed specification. (to be implemented)
 %% @end
 
-%% TODO: open source license:  Erlang Public License (MPL), 
 %% TODO: github
 %% TODO: review conditional compiler directives
 %% TODO: figure out module name clash strategy
