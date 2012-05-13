540b6d9462c0b7a8a9aa2e16fdb9c81e024bef04
Beads <beads@beadsland.com>
2012-05-13 12:46:29 -0400

    format hack errors

--- src/nosh.erl
+++ src/nosh.erl
@@ -147,7 +147,7 @@ do_run(IO, Line) ->
   case pose_code:load(IO, Command) of
     {module, Module}    -> CmdPid = spawn_link(Module, run, [?IO(self())]),
                            ?MODULE:loop(IO, Command, CmdPid);
-    Else                -> ?DEBUG("hack: ~p~n", [Else]),
+    Else                -> ?STDERR("hack: ~s~n", [?ERLERR_FORMAT(Else)]),
                            do_parse(IO, Line)
   end.
 
