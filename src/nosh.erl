aeffd5afed1fe8e44950cfeec13f46b8a6c09534
beadsland <beads.d.land@gmail.com>
2013-04-19 18:40:04 -0400

    refactor macros to library

--- src/nosh.erl
+++ src/nosh.erl
@@ -18,7 +18,7 @@
 %% by brackets replaced by your own identifying information:
 %% "Portions Copyright [year] [name of copyright owner]"
 %%
-%% Copyright 2012 Beads D. Land-Trujillo.  All Rights Reserved
+%% Copyright 2012, 2013 Beads D. Land-Trujillo.  All Rights Reserved.
 %% -----------------------------------------------------------------------
 %% CDDL HEADER END
 
@@ -79,8 +79,7 @@
 
 -define(debug, true).
 -include_lib("pose/include/interface.hrl").
-
--include("macro.hrl").
+-include_lib("pose/include/macro.hrl").
 
 -import(string).
 -import(re).
