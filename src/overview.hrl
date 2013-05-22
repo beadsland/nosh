96959cc72f471ed34bd6a5417fd2b1df1192773a
beadsland <beads.d.land@gmail.com>
2013-05-22 15:58:28 -0400

    copyright

--- src/overview.hrl
+++ src/overview.hrl
@@ -18,7 +18,7 @@
 %% by brackets replaced by your own identifying information:
 %% "Portions Copyright [year] [name of copyright owner]"
 %%
-%% Copyright 2012 Beads D. Land-Trujillo.  All Rights Reserved
+%% Copyright 2012, 2013 Beads D. Land-Trujillo.  All Rights Reserved.
 %% -----------------------------------------------------------------------
 %% CDDL HEADER END
 
